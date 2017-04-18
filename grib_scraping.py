734ce5820fc7ac976d44fbbc2f59bf59143ee168
Lemonella <widder3@yandex.ru>
2017-04-18 13:39:19 +0300

    Script that takes mushroom dictionary from file

--- grib_scraping.py
+++ grib_scraping.py
@@ -1,6 +1,13 @@
+'''
+This script scrapes data from Wikigrib and saves it in the file in JSON format.
+It also can extract it from file and convert to dictionary.
+Now only the second option is on.
+'''
+
 import requests
 from bs4 import BeautifulSoup
 import pprint
+import json
 
 def get_html(url):
 	'''
@@ -42,9 +49,7 @@ def get_section_info(url):
 
 	return mushrooms
 
-
-if __name__ == '__main__':
-
+def get_mushrooms_dictionary():
 	# Mushroom types for classification
 	sections = {
 		'Съедобные': 'http://wikigrib.ru/vidy/sedobnye-griby/',
@@ -63,4 +68,33 @@ if __name__ == '__main__':
 	for type in sections:
 		mushroom_dictionary[type] = get_section_info(sections[type])
 
-	pprint.pprint(mushroom_dictionary)
+	#pprint.pprint(mushroom_dictionary)	
+	return mushroom_dictionary
+
+
+def save_dictionary_to_json_file(dict):
+	json_dict = json.dumps(dict)
+	# json.loads(json) - converts json to dictionary
+
+	f = open('mushrooms.txt', 'w')
+	f.write(json_dict) 
+	f.close()
+
+	#with open (“mushrooms.txt”, “w”) as file: 
+	#	file.write(json) 
+
+
+def get_mushrooms_from_json_file():
+	f = open('mushrooms.txt', 'r')
+	json_dict = f.read() 
+	f.close()
+
+	dictionary = json.loads(json_dict)
+	return dictionary
+
+
+if __name__ == '__main__':
+
+	# mushroom_dictionary = get_mushrooms_dictionary()
+	# save_dictionary_to_json_file(mushroom_dictionary)
+	pprint.pprint(get_mushrooms_from_json_file())
